05e918beac56728a01b0861bf8551efbfcff3241
RaggerLtp <ragger.break@gmail.com>
2020-11-23 20:02:47 +0100

    validation adding word

--- dictionary_pam/lib/addWord.dart
+++ dictionary_pam/lib/addWord.dart
@@ -5,7 +5,6 @@ import 'dbhelper.dart';
 
 class AddWord extends StatefulWidget {
 
-
   @override
   _AddWordPageState createState() => _AddWordPageState();
 
@@ -13,50 +12,87 @@ class AddWord extends StatefulWidget {
 
 class _AddWordPageState extends State<AddWord> {
 
-  TextEditingController wordEngController = new TextEditingController();
-  TextEditingController wordPlController = new TextEditingController();
-  TextEditingController descriptionController = new TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text("Dodaj nowe słowo"),
         ),
-        body: Column(
-          children: [
-            TextField(
-                controller: wordEngController,
-                decoration: InputDecoration(
-                  labelText: 'Słówko'
-          )),
-            TextField(
-                controller: wordPlController,
-                decoration: InputDecoration(
-                    labelText: 'Tłumaczenie'
-            )),
-            TextField(
-                controller: descriptionController,
-                decoration: InputDecoration(
-                    hintText: 'Opcjonalnie',
-                    labelText: 'Opis'
-            )),
-            MaterialButton(
-              onPressed: () {
-                var helper = DbHelper();
-                helper.insertWord(new Word(
-                    id: 0,
-                    wordEng: wordEngController.text,
-                    wordPl: wordPlController.text,
-                    description: descriptionController.text));
-                Navigator.pop(context);
-              },
-              child: IconButton(
-                icon: new Icon(Icons.add)),
-            )
-          ],
-        )
+        body: MyCustomForm(),
     );
   }
+}
 
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+class MyCustomFormState extends State<MyCustomForm> {
+
+  TextEditingController wordEngController = new TextEditingController();
+  TextEditingController wordPlController = new TextEditingController();
+  TextEditingController descriptionController = new TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+              controller: wordEngController,
+              decoration: InputDecoration(
+                  labelText: 'Słówko'
+              ),
+                validator: (value) {
+                if (value.isEmpty) {
+                  return 'Proszę podać słówko';
+                }
+                return null;
+              }),
+          TextFormField(
+              controller: wordPlController,
+              decoration: InputDecoration(
+                  labelText: 'Tłumaczenie'
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Proszę podać tłumaczenie';
+                }
+                return null;
+              }),
+          TextFormField(
+              controller: descriptionController,
+              decoration: InputDecoration(
+                  hintText: 'Opcjonalnie',
+                  labelText: 'Opis'
+              )),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState.validate()) {
+                  var helper = DbHelper();
+                  helper.insertWord(new Word(
+                      id: 0,
+                      wordEng: wordEngController.text,
+                      wordPl: wordPlController.text,
+                      description: descriptionController.text));
+                  Navigator.pop(context);
+                }
+              },
+              child: Icon(
+                Icons.add,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
